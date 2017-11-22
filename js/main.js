87bb2fb237ef1ab12c8b2d8bac42c3482d0e103f
cathy19 <chencathy19@gmail.com>
2017-11-22 14:52:57 +0800

    no
    no

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*
+* @Author: cathy.chen
+* @Date:   2017-03-28 15:03:46
+* @Last Modified by:   cathy.chen
+* @Last Modified time: 2017-03-30 14:11:10
+*/
+$(function(){
+
+	$('#ad-carousel').carousel({
+		interval:2500,
+	})
+})
+'use strict';
+
+
+
